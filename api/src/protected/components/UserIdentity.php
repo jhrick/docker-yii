6ee2efad0b715adeec489c695664228a204d6ef1
Rick <hj.rickcp@gmail.com>
2025-05-03 08:10:21 -0300

    refactor(arch): minor changes

--- api/src/protected/components/UserIdentity.php
+++ api/src/protected/components/UserIdentity.php
@@ -4,11 +4,11 @@ class UserIdentity extends CUserIdentity
 {
 	public function authenticate()
 	{
-		$users=array(
+		$users=[
 			// username => password
 			'demo'=>'demo',
 			'admin'=>'admin',
-		);
+		];
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
 		elseif($users[$this->username]!==$this->password)
